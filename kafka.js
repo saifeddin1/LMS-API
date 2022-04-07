6e1bacd8ba707fdccc97b051c4348b135504a0a5
Tarek chafroud <chafroudtarek6@gmail.com>
2022-04-07 12:46:18 +0100

    add kafka

--- kafka.js
+++ kafka.js
@@ -9,44 +9,35 @@ const kafka = new Kafka({
     brokers: ['localhost:9092', 'localhost:9092']
   })
   
-  const consumer = kafka.consumer({ groupId: 'kafka' })
+  const consumer = kafka.consumer({ groupId: 'kafka1' })
   
   
 const run = async (req ,res) => {
     
     await consumer.connect()
-    await consumer.subscribe({ topic: 'test', fromBeginning: true })
+    await consumer.subscribe({ topic: 'my-topic1'})
+   
   
     await consumer.run({
+     
       eachMessage: async ({ topic, partition, message }) => {
-        // console.log({
-        //   partition,
-        //   offset: message.offset,
-        //   value: message.value,
         
-        // })
        
         
         const obj = JSON.parse(message.value);
-        console.log(obj)
+        
         try{
-            //const user = await User.findOne({email: obj.email});
-           // if(user){
-              //   if(obj.enabled == false){
-              //       user.enabled = false;
-              //       await user.save();
-              //       console.log("successfully updated")
-              //   }else{
+            
               const response= await User.updateOne( {"_id": ObjectId(obj._id)}, {$set:obj}, {upsert: true})
               
               if(response){
-                  console.log("send message successfully...")
+                  console.log("Executing wihout problems...")
               }
               
                 }catch(e){
                     console.log("catch : "+e)
                 }
-          //  }
+        
        
       },
     })
